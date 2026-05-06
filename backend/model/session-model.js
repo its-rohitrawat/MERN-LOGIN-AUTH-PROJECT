e2d9fb8711c173feb26df4d562607080c0e4de5f
its-rohitrawat <rohit98rawat96@gmail.com>
2026-05-06 16:02:43 +0530

    verification controller and login completed

---
+++ backend/model/session-model.js
@@ -0,0 +1,11 @@
+import mongoose  from "mongoose";
+
+
+const SessionSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+})
+
+export const Session = mongoose.model("session", SessionSchema)
